240e2ae4a17e563c0489ac022631f585102951d0
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-05-18 17:45:54 +0200

    project started

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 import "./style/scss/_custom.scss";
+import "./todo.js";
+import { ToDoList } from "./todo.js";
 // You can specify which plugins you need
 import * as all from "bootstrap";
+import { createPopper } from "@popperjs/core";
 // import { Tooltip, Toast, Popover } from "bootstrap";
-console.log(all);
 
-// import retrieveContent from "query.js";
+// DOM
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input_article = document.querySelector("#input-article");
+const input_categorie = document.querySelector("#input-categorie");
+console.log(form);
 
-// async function showContent() {
-//   try {
-//     const content = await retrieveContent();
+if (document.readyState === "loading") {
+  window.addEventListener("DOMContentLoaded", pageReady);
+} else {
+  const pageReady = () => {
+    const todolist = new ToDoList(ul);
+    console.log(todolist);
 
-//     const elt = document.createElement("div");
-//     elt.innerHTML = content.join("<br/>");
-//     document.getElementsByTagName("body")[0].appendChild(elt);
-//   } catch (e) {
-//     console.log("Error", e);
-//   }
-// }
-
-// showContent();
+    // EventListener
+    const addWork = () => {
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (input_article.value || input_categorie.value) {
+          input_article.value = "";
+          input_categorie.value = "";
+        } else {
+          if (input_article.value === "" || input_categorie.value === "") {
+            alert("Veuillez ajouter un article et catégorie s'il vous plait");
+          }
+        }
+      });
+    };
+    addWork();
+  };
+  pageReady();
+}
