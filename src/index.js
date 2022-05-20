7f24945c2aefa6732ca491e857b846fbcc2bab20
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-05-20 14:25:12 +0200

    navbar and fontawesome installed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./style/scss/_custom.scss";
 import "./todo.js";
+
 import { ToDoList } from "./todo.js";
 // You can specify which plugins you need
 import * as all from "bootstrap";
-import { createPopper } from "@popperjs/core";
+import { arrow, createPopper } from "@popperjs/core";
 // import { Tooltip, Toast, Popover } from "bootstrap";
 
 // DOM
@@ -18,23 +19,43 @@ if (document.readyState === "loading") {
 } else {
   const pageReady = () => {
     const todolist = new ToDoList(ul);
-    console.log(todolist);
 
     // EventListener
-    const addWork = () => {
+    const eventListener = () => {
       form.addEventListener("submit", (event) => {
         event.preventDefault();
-        if (input_article.value || input_categorie.value) {
-          input_article.value = "";
-          input_categorie.value = "";
-        } else {
-          if (input_article.value === "" || input_categorie.value === "") {
-            alert("Veuillez ajouter un article et catégorie s'il vous plait");
-          }
-        }
+        updateToDoList();
+      });
+
+      form.addEventListener("dblclick", (event) => {
+        event.preventDefault();
+        updateToDoList();
       });
     };
-    addWork();
+
+    const updateToDoList = () => {
+      const articleValue = input_article.value;
+      // L'idée est d'envoyer la valeur de l'input à update pour pouvoir faire le CRUD en updatant à chaque fois
+      todolist.updateToDoList(articleValue);
+
+      if (input_article.value) {
+        console.log(input_article.value);
+        input_article.value = "";
+        input_categorie.value = "";
+      } else {
+        // Pour dire si l'article vide, on ne peut pas submitter
+        if (input_article.value === "") {
+          alert("Veuillez ajouter un article et catégorie s'il vous plait");
+        }
+      }
+      // else {
+      //   // Pour dire si l'article vide, on ne peut pas submitter
+      //   if (input_article.value === "" || input_categorie.value === "") {
+      //     alert("Veuillez ajouter un article et catégorie s'il vous plait");
+      //   }
+      // }
+    };
+    eventListener();
   };
   pageReady();
 }
--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,67 @@
 // Créer la classe todolist
 class ToDoList {
-  constructor(_text) {
-    this.toDoListText = _text;
-    this.arrayText = [];
+  constructor(_list) {
+    this.toDoListText = _list;
+    this.arrayText = [{ text: "Je suis un todo", done: true, editMode: false }];
     this.idCompleted = false;
   }
 
-  showText() {
-    console.log(this.arrayText, this.toDoListText);
+  updateToDoList(text) {
+    // displayTodo
+    console.log(text);
+    // for (const [index, value] of this.arrayText.entries()) {
+    //   console.log(index, value);
+    // }
+
+    const todoArray = this.arrayText.map((todo, index) => {
+      console.log(todo, index, text);
+      if (todo.editMode) {
+        return this.modifierToDoElement(todo, index);
+      } else {
+        return this.creerToDoElement(todo, index);
+      }
+    });
+
+    toDoListText.innerHTML = "";
+    returntoDoListText.append(...todoArray);
   }
+
+  //   static creerToDoElement(text) {
+  //     const li = document.createElement("li");
+  //     const p = document.createElement("p");
+  //     const deleteButton = document.createElement("button");
+  //     const modifcationButton = document.createElement("button");
+  //     deleteButton.innerText = "Supprimer";
+  //     modifcationButton.innerText = "Modifier";
+  //     deleteButton.classList.add("danger");
+  //     deleteButton.classList.add("modification");
+
+  //     li.innerHTML = `
+  //     <span class="todo"></span>
+  //     `;
+  //     p.innerHTML = `${
+  //       text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+  //     }`;
+
+  //     li.style.cursor = "pointer";
+
+  //     li.append(p, modifcationButton, deleteButton);
+
+  //     return li;
+  //   }
+  //   createToDo(text) {
+  //     console.log(this.arrayText.push(text));
+  //     this.toDoListText.append(ToDoList.creerToDoElement(text));
+  //     this.updateToDoList();
+  //   }
+
+  //   effacerToDo() {
+  //     this.updateToDoList();
+  //   }
+
+  //   modifierToDoElement() {
+  //     updateToDoList();
+  //   }
 }
 
 // Export
