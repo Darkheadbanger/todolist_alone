23b922dc2a53a8098d0bdc6a1d12f3d1e7ef02a1
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-06-11 14:11:08 +0200

    read done in es6 way

--- src/index.js
+++ src/index.js
@@ -8,98 +8,12 @@ import * as all from "bootstrap";
 import { arrow, createPopper } from "@popperjs/core";
 // import { Tooltip, Toast, Popover } from "bootstrap";
 
-// DOM
-const ul = document.querySelector("ul");
-const formAdd = document.querySelector("#formAdd");
-const inputArticle = document.querySelector("#input-article");
-const inputdescription = document.querySelector("#input-description");
-const inputCategorie = document.querySelector("#input-categorie");
-const inputQuantity = document.querySelector("#input-quantity");
-const addArticleError = document.querySelector(".add-article-error");
-const addCategorieError = document.querySelector(".add-categorie-error");
-const addquantityError = document.querySelector(".add-quantity-error");
-
 if (document.readyState === "loading") {
   window.addEventListener("DOMContentLoaded", pageReady);
 } else {
   const pageReady = () => {
     const todolist = new ToDoList();
     console.log(todolist);
-    const eventListenerAddArticle = () => {
-      // EventListener
-      formAdd.addEventListener("submit", (event) => {
-        event.preventDefault();
-        updateToDo(); // Ou updateToDoList à voir
-      });
-      formAdd.addEventListener("dbclick", (event) => {
-        event.preventDefault();
-        updateToDo(); // Ou updateToDoList à voir
-      });
-      formAdd.addEventListener(
-        "blur",
-        (event) => {
-          event.target.style.color = "white";
-        },
-        true
-      );
-      formAdd.addEventListener(
-        "focus",
-        (event) => {
-          console.log(event.target);
-        },
-        true
-      );
-      formAdd.addEventListener(
-        "click",
-        (event) => {
-          console.log(event.target);
-        },
-        true
-      );
-    };
-
-    const updateToDo = () => {
-      const inputAddValue = [
-        inputArticle.value,
-        inputdescription.value,
-        inputCategorie.value,
-        inputQuantity.value,
-      ];
-      //   todolist.updateToDoList(inputAddValue);
-      filledInputAddToZero(inputAddValue);
-      errorInputAdd(inputAddValue);
-    };
-    const filledInputAddToZero = (inputAddValue) => {
-      if (inputAddValue) {
-        inputArticle.value = "";
-        inputdescription.value = "";
-        inputCategorie.value = "";
-        inputQuantity.value = "";
-        addArticleError.classList.add("add-article-error");
-        addCategorieError.classList.add("add-categorie-error");
-        addquantityError.classList.add("add-quantity-error");
-      }
-    };
-
-    const errorInputAdd = (inputAddValue) => {
-      console.log(
-        inputAddValue[0],
-        inputAddValue[1],
-        inputAddValue[2],
-        inputAddValue[3]
-      );
-      if (
-        inputAddValue[0] === "" &&
-        inputAddValue[1] === "" &&
-        inputAddValue[2] === "" &&
-        inputAddValue[3] === ""
-      ) {
-        addArticleError.classList.remove("add-article-error");
-        addCategorieError.classList.remove("add-categorie-error");
-        addquantityError.classList.remove("add-quantity-error");
-      }
-    };
-    eventListenerAddArticle();
   };
   pageReady();
 }
